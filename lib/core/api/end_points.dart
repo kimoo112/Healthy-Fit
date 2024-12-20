63e7ed1879bb66ba6d88aa48bec2a2972e278bf3
kimoo112 <kimo8123008@gmail.com>
2024-12-20 15:24:27 +0200

    Add API configuration and Dio setup files -Added API configuration files for managing endpoints and API keys. -Integrated Dio for handling requests. -Configured Dio interceptors for request/response logging and error handling. -Improved API structure for scalability and reusability.

---
+++ lib/core/api/end_points.dart
@@ -0,0 +1,18 @@
+class EndPoint {
+  static String baseUrl = "https://rcipe-4lfy.vercel.app/api/";
+  static String login = "users/login";
+  static String register = "users/register";
+}
+
+class ApiKeys {
+  static String status = "status";
+  static String errorMessage = "message";
+  static String email = "email";
+  static String password = "password";
+  static String token = "token";
+  static String message = "message";
+  static String name = "name";
+  static String age = "age";
+  static String height = "height";
+  static String weight = "weight";
+}
---
+++ lib/core/api/error_model.dart
@@ -0,0 +1,13 @@
+import 'end_points.dart';
+
+class ErrorModel {
+  final String errorMessage;
+
+  ErrorModel(
+      { required this.errorMessage});
+  factory ErrorModel.fromJson(Map<String, dynamic> jsonData) {
+    return ErrorModel(
+      errorMessage: jsonData[ApiKeys.errorMessage],
+    );
+  }
+}
